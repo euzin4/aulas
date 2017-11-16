3755878c2e924a18877680212ec0ce50ce974fe1
kevin willian <tste@outlook.com.br>
2017-11-16 16:54:34 -0200

    triangulo.c

--- trabalhos/triangulo.c
+++ trabalhos/triangulo.c
@@ -4,8 +4,8 @@ typedef struct{
 	int x, y;
 }ponto;
 
-	/*|a.x  a.y 1|a.x  a.y       	a=(1,1)
-	  |b.x  b.y 1|b.x  b.y 		b=(1,4)
+	/*|a.x  a.y 1|a.x  a.y		a=(1,1)
+	  |b.x  b.y 1|b.x  b.y		b=(1,4)
 	  |c.x  c.y 1|c.x  c.y		c=(4,1)    */
 int calculaArea(ponto a, ponto b, ponto c){
 	int A;
@@ -14,40 +14,37 @@ int calculaArea(ponto a, ponto b, ponto c){
 }
 
 int main(){
-	ponto a;
-	ponto b;
-	ponto c;
-	ponto p;
-	int i;
-	int j;
-	scanf("%d",&a.x);
-	scanf("%d",&a.y);
-	scanf("%d",&b.x);
-	scanf("%d",&b.y);
-	scanf("%d",&c.x);
-	scanf("%d",&c.y);
-	int area = calculaArea(a, b, c);
-	if(area < 0)
+	ponto a,b,c,p;
+	int i,j,area,AB,AC,BC;
+
+	scanf("%d %d %d %d %d %d",&a.x,&a.y,&b.x,&b.y,&c.x,&c.y);
+	area = calculaArea(a, b, c);
+	if(area < 0){
 		area = -area;
+    }
 	printf("area: %d\n", area);
-	int AB, AC, BC;
 	for(i = 0; i < 20; i++){
 		for(j = 0; j < 20; j++){
-		p.x = i;
-		p.y = j;
-		AB = calculaArea(a, b, p);
-		if(AB < 0)
-			AB = -AB;
-		AC = calculaArea(a, c, p);
-		if(AC < 0)
-			AC = -AC;
-		BC = calculaArea(b, c, p);
-		if(BC < 0)
-			BC = -BC;
-		if(AB + AC + BC == area)
-			printf("* ");
-		else
-			printf("  ");
+            p.x = i;
+            p.y = j;
+            AB = calculaArea(a, b, p);
+            if(AB < 0){
+                AB = -AB;
+            }
+            AC = calculaArea(a, c, p);
+            if(AC < 0){
+                AC = -AC;
+            }
+            BC = calculaArea(b, c, p);
+            if(BC < 0){
+                BC = -BC;
+            }
+            if(AB + AC + BC == area){
+                printf("* ");
+            }
+            else{
+                printf("  ");
+            }
 		}
 		printf("\n");
 	}
