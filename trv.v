36513c8c27879f3151dd3e4a9099511e96b013a7
kevin willian <tste@outlook.com.br>
2017-12-02 23:01:38 -0200

    Update trv.v

--- trv.v
+++ trv.v
@@ -3,81 +3,81 @@ input [11:0] ptAX,
 input [11:0] ptAY,
 input [11:0] ptBX,
 input [11:0] ptBY,
-input [11:0] pttX,
-input [11:0] pttY,
+input [11:0] ptCX,
+input [11:0] ptCY,
 output trv);
-wire signed [11:0] sbA;
-wire signed [11:0] sbB;
-wire signed [11:0] sbC;
-wire signed [11:0] sbD;
-wire signed [22:0] mpA;
-wire signed [22:0] mpB;
-wire signed [22:0] sbA;
-assign sbA = pttX - ptBY;
-assign sbB = ptAY - ptBY;
-assign sbC = ptAX - ptBX;
-assign sbD = pttY - ptBY;
-assign mpA = sbA * sbB;
-assign mpB = sbC * sbD;
-assign sbA = mpA - mpB;
-assign trv = (sbA >= 0) ? 1 : 0;
+wire signed [11:0] sub1;
+wire signed [11:0] sub2;
+wire signed [11:0] sub3;
+wire signed [11:0] sub4;
+wire signed [22:0] mp1;
+wire signed [22:0] mp2;
+wire signed [22:0] compr;
+assign sub1 = ptCX - ptBY;
+assign sub2 = ptAY - ptBY;
+assign sub3 = ptAX - ptBX;
+assign sub4 = ptCY - ptBY;
+assign mp1 = sub1 * sub2;
+assign mp2 = sub3 * sub4;
+assign compr = mp1 - mp2;
+assign trv = (compr >= 0) ? 1 : 0;
 endmodule
 
-module tstrig (
+module tstTR (
 input [11:0] ptAX,
 input [11:0] ptAY,
 input [11:0] ptBX,
 input [11:0] ptBY,
 input [11:0] pt3X,
 input [11:0] pt3Y,
-input [11:0] pttX,
-input [11:0] pttY,
+input [11:0] ptCX,
+input [11:0] ptCY,
 output ins);
-wire trv1;
-wire trv2;
-wire trv3;
-assign ins = (trv1 == 1 && trv2 == 1 && trv3 == 1) ? 1:0;
-trv S1(ptAX, ptAY, ptBX, ptBY, pttX, pttY, trv1);
-trv S2(ptBX, ptBY, pt3X, pt3Y, pttX, pttY, trv2);
-trv S3(pt3X, pt3Y, ptAX, ptAY, pttX, pttY, trv3);
+wire sig1;
+wire sig2;
+wire sig3;
+assign ins = (sig1 == 1 && sig2 == 1 && sig3 == 1) ? 1:0;
+trv S1(ptAX, ptAY, ptBX, ptBY, ptCX, ptCY, sig1);
+trv S2(ptBX, ptBY, pt3X, pt3Y, ptCX, ptCY, sig2);
+trv S3(pt3X, pt3Y, ptAX, ptAY, ptCX, ptCY, sig3);
 endmodule
 
-module Teste;
+module tste;
 reg [11:0] ptAX;
 reg [11:0] ptAY;
 reg [11:0] ptBX;
 reg [11:0] ptBY;
 reg [11:0] pt3X;
 reg [11:0] pt3Y;
-reg [11:0] pttX;
-reg [11:0] pttY;
-wire Dentro;
-tstrig A(ptAX, ptAY, ptBX, ptBY, pt3X, pt3Y, pttX, pttY, Dentro);
+reg [11:0] ptCX;
+reg [11:0] ptCY;
+wire dentro;
+tstTR A(ptAX, ptAY, ptBX, ptBY, pt3X, pt3Y, ptCX, ptCY, dentro);
 initial
-        begin
-           $dumpvars(0,A);
-           #1
-           ptAX <= 10;
-           ptAY <= 10;
-           ptBX <= 30;
-           ptBY <= 10;
-           pt3X <= 20;
-           pt3Y <= 30;
-           pttX <= 15;
-           pttY <= 15;
-           #1
-           pttX <= 15;
-           pttY <= 15;
-           #1
-           pttX <= 9;
-           pttY <= 15;
-           #1
-           pttX <= 10;
-           pttY <= 11;
-           #1
-           pttX <= 30;
-           pttY <= 11;
-           #40
-           $finish;
-        end
+    begin
+        $dumpvars(0,A);
+        #1
+        ptAX <= 10;
+        ptAY <= 10;
+        ptBX <= 30;
+        ptBY <= 10;
+        pt3X <= 20;
+        pt3Y <= 30;
+        ptCX <= 15;
+        ptCY <= 15;
+        #1
+        ptCX <= 15;
+        ptCY <= 15;
+        #1
+        ptCX <= 9;
+        ptCY <= 15;
+        #1
+        ptCX <= 10;
+        ptCY <= 11;
+        #1
+        ptCX <= 30;
+        ptCY <= 11;
+        #40
+        $finish;
+    end
 endmodule
